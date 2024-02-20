2cc626c8ba26846eefbd062a4387918d5504b891
steven gonsalvez <steven.gonsalvez@gmail.com>
2024-02-20 22:44:21 +0000

    fix: removed unused storage obj

--- src/talktothecode.py
+++ src/talktothecode.py
@@ -3,8 +3,6 @@ from src.llm import  vectordb, llm_manager
 from config import codebase_llm
 
 llm_mgr = llm_manager.LLMManager(config=codebase_llm)
-storage_obj = vectordb.VectordbManager(llm_manager=llm_mgr)
-
 
 test = CodebaseInteraction(llm_manager=llm_mgr,repo_url="https://github.com/stevengonsalvez/makemynewsletter")
 query = "How do I refactor the app and implement poetry for dependency management?"
